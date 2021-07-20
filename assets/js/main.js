f15c9e2b8fa38c7efdb4fedd2a05b7b0538e70cc
srbhr <srbh077@gmail.com>
2021-07-20 23:25:58 +0530

    Modified and Started Developing the Main Page index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,22 @@ function switch_dark_mode_icon(darkmode, id) {
     darkmode.toggle();
     render_dark_mode_icon(darkmode, id);
 }
-const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-}
+// const options = {
+//     bottom: '64px', // default: '32px'
+//     right: 'unset', // default: '32px'
+//     left: '32px', // default: 'unset'
+//     time: '0.5s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//     backgroundColor: '#2C2E43',  // default: '#fff'
+//     buttonColorDark: '#100f2c',  // default: '#100f2c'
+//     buttonColorLight: '#fff', // default: '#fff'
+//     saveInCookies: false, // default: true,
+//     label: '🌓', // default: ''
+//     autoMatchOsTheme: true // default: true
+// }
 
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
+// const darkmode = new Darkmode(options);
+// darkmode.showWidget();
 
 function render_dark_mode_icon(darkmode, id) {
     var x = document.getElementsByTagName("img");
